9a077ef22a4f2d898c013804e85aa1792154bc41
JarateKing <scmacintosh@upei.ca>
2022-05-11 07:53:27 -0300

    updated script to work with new commit graph

--- Github Commit Graphs Improved.user.js
+++ Github Commit Graphs Improved.user.js
@@ -1,24 +1,24 @@
 // ==UserScript==
 // @name         Github Commit Graphs Improved
 // @namespace    https://github.com/JarateKing
-// @version      1.2
+// @version      1.3
 // @description  Improve Github's commit graphs
 // @include      https://github.com/*
 // @exclude      https://github.com/
 // @exclude      https://github.com/*/*
 // ==/UserScript==
 
-const percent_exponent = 0.7;
+const percent_exponent = 0.6;
 
-const color1 = [198, 228, 138];
-const color2 = [123, 201, 111];
-const color3 = [35, 154, 59];
-const color4 = [25, 97, 39];
+const color1 = [155, 233, 168];
+const color2 = [64, 196, 99];
+const color3 = [48, 161, 78];
+const color4 = [33, 110, 57];
 
 (function() {
     'use strict';
 
-    var elements = document.getElementsByClassName("day");
+    var elements = document.getElementsByClassName("ContributionCalendar-day");
     var maxCommits = 0;
     for (var i = 0; i < elements.length; i++)
     {
@@ -29,17 +29,20 @@ const color4 = [25, 97, 39];
     }
     for (i = 0; i < elements.length; i++)
     {
-        if (parseInt(elements[i].getAttribute("data-count")) == 0)
+        if (elements[i].getAttribute("data-count") !== null)
         {
-           elements[i].setAttribute("fill", "#EEEEEE");
-        }
-        else if (maxCommits == 1)
-        {
-            elements[i].setAttribute("fill", "#C6E48A");
-        }
-        else
-        {
-            elements[i].setAttribute("fill", getColor(elements[i].getAttribute("data-count"),maxCommits));
+            if (parseInt(elements[i].getAttribute("data-count")) == 0)
+            {
+                elements[i].setAttribute("style", "fill: #EBEDF0;");
+            }
+            else if (maxCommits == 1)
+            {
+                elements[i].setAttribute("style", "fill: #9BE9A8;");
+            }
+            else
+            {
+                elements[i].setAttribute("style", "fill: " + getColor(elements[i].getAttribute("data-count"),maxCommits) + ";");
+            }
         }
     }
 })();
